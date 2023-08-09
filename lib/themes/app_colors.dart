d5678d498ad9781b047186117d133652236fbcec
Anvesh Mishra <anveshcr7@gmail.com>
2023-08-09 22:06:12 +0530

    Add files via upload

---
+++ lib/themes/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_5/hexcolor/hexcolor.dart';
+
+final Color adaniLightPrimaryColor = HexColor('#0D67CA');
+final Color adaniLightDividerColor = HexColor('#E2E2E2');
+final Color adaniLightBorderColor = HexColor('#E2E2E2');
+final Color adaniLightSecondaryColor = HexColor('#666666');
+final Color adaniLightTextColor = HexColor('#222222');
+final Color adaniLightBackGroundColor = HexColor('#FFFFFF');
+final Color errorColor = HexColor('#FF0000');
